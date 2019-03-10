f98284e5448a688764fa96f53db1ea2b70d7542d
nguye225 <46941605+nguye225@users.noreply.github.com>
2019-03-09 21:08:19 -0500

    add play rules of the Go Fish Game

--- src/ca/sheridancollege/project/Game.java
+++ src/ca/sheridancollege/project/Game.java
@@ -10,6 +10,13 @@ import java.util.ArrayList;
 /**
  * The class that models your game. You should create a more specific
  * child of this class and instantiate the methods given.
+ * We create Go Fish Cards Game, which contain 52 cards and at least 2 or more players 
+ * The rules is, if there are 2 players each of them will get 7 cards, if there are 3 or more players 
+ * each of them will get 5 cards and the remaining cards place in the middle . One of the player will
+ * call the name of the card that person has if all others players have it then should give it to the 
+ * first player for those cards that player put it down and then keep going to next player until 
+ * they are all run out of cards. Final step that all players count how many cards each of them have
+ * the player has the most cards is the winner.
  * @author dancye, 2018
  */
 public abstract class Game 
